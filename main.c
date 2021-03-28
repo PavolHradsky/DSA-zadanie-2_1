b47f70c35c83fad271939177b8a32ac707819ccc
Pavol Hradsky <hradskypalo@gmail.com>
2021-03-28 18:07:15 +0200

    hotovo

--- main.c
+++ main.c
@@ -6,27 +6,35 @@
 ************************************/
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct node {
     unsigned int key;
-    char* data;
+    unsigned int data;
     struct node *next;
 } NODE;
 
-#define SIZE 2000
+#define SIZE 50000
 NODE table[SIZE];
 
-unsigned int hash(char *str) {
+unsigned int hash(unsigned int num) {
     unsigned int h = 0;
-    for (int i = 0; i < strlen(str); ++i) {
-        h = 31 * h + str[i];
-    }
+//    for (int i = 0; i < strlen(str); ++i) {
+//        h = 31 * h + str[i];
+//    }
+//    return h % SIZE;
+
+    do {
+        h = 31 * h + (num%10);
+        num /= 10;
+    } while (num/10 != 0);
+    h = 31 * h + (num%10);
     return h % SIZE;
+
 }
 
-NODE *create_node(char* data) {
+NODE *create_node(unsigned int data) {
     NODE *result = NULL;
     result = (NODE*)malloc(sizeof(NODE));
     result->data = data;
@@ -36,8 +44,8 @@ NODE *create_node(char* data) {
 }
 
 
-void insert(char *str) {
-    NODE *toInsert = create_node(str);
+void insert(unsigned int num) {
+    NODE *toInsert = create_node(num);
     unsigned int index = toInsert->key;
 
     if(table[index].next == NULL) {
@@ -52,13 +60,13 @@ void insert(char *str) {
 
 }
 
-void search(char *str) {
-    unsigned int index = hash(str);
+void search(unsigned int num) {
+    unsigned int index = hash(num);
     NODE *tmp = table[index].next;
-    while (strcmp(str, tmp->data) != 0) {
+    while (num != tmp->data) {
         tmp = tmp->next;
     }
-    printf("Finded %s with key %d\n", tmp->data, tmp->key);
+    //printf("Finded %d with key %d\n", tmp->data, tmp->key);
 }
 
 void print_table() {
@@ -67,7 +75,7 @@ void print_table() {
             NODE *tmp = table[i].next;
             printf("index %d -", i);
             do {
-                printf(" %s -", tmp->data);
+                printf(" %d, ", tmp->data);
                 tmp = tmp->next;
             } while (tmp != NULL);
             printf("\n");
@@ -78,16 +86,26 @@ void print_table() {
 
 int main(void) {
 
-    insert("palo");
-    insert("ondrej");
-    insert("slovo");
-    insert("em");
-    insert("hahahahah");
-    insert("vetb");
-    insert("ah");
+    int max = 1000000;
+    int toFind = 500000;
+    clock_t start, end;
+    double cpu_time_used;
 
-    print_table();
+    start = clock();    //zapne casovac
 
-    search("palo");
+    for (int i = 0; i < max; ++i) {     //vytvori strom z max cisel (1000000)
+        insert(i);
+    }
+    int num;
+    for (int i = 0; i < toFind; ++i) {  //hlada nahodne cisla od 0 po max v strome toFind-krat (500000)
+        num = (rand() % (max));
+        search(num);
+    }
+
+    end = clock();      //skonci casovac
+    cpu_time_used = ((double) (end - start));   //vyrata cas
+    printf("finding %d times in tree size %d\n", toFind, max);      //vypise vysledok
+    printf("Time: %dms\n", (int) cpu_time_used);
+
     return 0;
 }
